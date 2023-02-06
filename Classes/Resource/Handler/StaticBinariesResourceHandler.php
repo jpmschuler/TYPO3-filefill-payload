933957b8a49a161bb13b997ea7fd8af519268c99
J. Peter M. Schuler <j.peter.m.schuler@uni-due.de>
2023-02-06 23:28:42 +0100

    [TASK] cleanup

--- Classes/Resource/Handler/StaticBinariesResourceHandler.php
+++ Classes/Resource/Handler/StaticBinariesResourceHandler.php
@@ -2,14 +2,12 @@
 
 namespace Jpmschuler\FilefillPayload\Resource\Handler;
 
+use IchHabRecht\Filefill\Resource\RemoteResourceInterface;
 use TYPO3\CMS\Core\Resource\FileInterface;
-use TYPO3\CMS\Core\TypoScript\Parser\TypoScriptParser;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
-class StaticBinariesResourceHandler implements \IchHabRecht\Filefill\Resource\RemoteResourceInterface
+class StaticBinariesResourceHandler implements RemoteResourceInterface
 {
-    protected $configuration;
-
     protected $handledExtList = [];
 
     protected const AVAILABLE_EXT_LIST = ['7z', 'avi', 'ext', 'flv', 'mov', 'mp4', 'pdf', 'rar', 'rm', 'webm', 'zip'];
@@ -17,14 +15,19 @@ class StaticBinariesResourceHandler implements \IchHabRecht\Filefill\Resource\Re
 
     public function __construct($configuration)
     {
-       if (!$configuration) {
-           $this->handledExtList = self::AVAILABLE_EXT_LIST;
-       }
-       else {
-           $this->handledExtList = $configuration;
-       }
+        if (!$configuration) {
+            $this->handledExtList = self::AVAILABLE_EXT_LIST;
+        } else {
+            $this->handledExtList = $configuration;
+        }
     }
 
+    /**
+     * @param string $fileIdentifier
+     * @param string $filePath
+     * @param FileInterface|null $fileObject
+     * @return bool
+     */
     public function hasFile($fileIdentifier, $filePath, FileInterface $fileObject = null)
     {
         $fileInfo = pathinfo($fileIdentifier);
@@ -34,11 +37,19 @@ class StaticBinariesResourceHandler implements \IchHabRecht\Filefill\Resource\Re
         return false;
     }
 
+    /**
+     * @param string $fileIdentifier
+     * @param string $filePath
+     * @param FileInterface|null $fileObject
+     * @return resource|string
+     */
     public function getFile($fileIdentifier, $filePath, FileInterface $fileObject = null)
     {
         $fileInfo = pathinfo($fileIdentifier);
         if (in_array($fileInfo['extension'], $this->handledExtList, true)) {
-            return file_get_contents(GeneralUtility::getFileAbsFileName(self::PATH_PREFIX . '.' . $fileInfo['extension']));
+            return file_get_contents(
+                GeneralUtility::getFileAbsFileName(self::PATH_PREFIX . '.' . $fileInfo['extension'])
+            );
         }
         return '';
     }
